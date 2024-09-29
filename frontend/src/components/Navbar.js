80dee57e88420636b721e529cda4cf08b09fbce3
AngadDogra <angaddogra1204@gmail.com>
2024-09-29 23:03:04 +0530

    (feat) added navabar background

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // Import the CSS for styling the navbar
+import navBackground from '../pages/Assets/green-sky.jpg';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar" style={{ "--background-image": `url(${navBackground})` }}>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
